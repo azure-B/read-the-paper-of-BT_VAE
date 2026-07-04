432826472d602657c940eb176f58f81fafb09c6d
Azure_B <benefit5758@naver.com>
2026-07-04 17:20:16 +0900

    [2026.07.04] 효율 코딩 이전

--- BT_VAE/BT_VAE.py
+++ BT_VAE/BT_VAE.py
@@ -2,26 +2,30 @@ import torch
 import torch.nn as nn
 import numpy as np
 import torch.nn.functional as F
+# import glob
 
 class BT_VAE(nn.Module):
-    def __init__(self, z_dim=32):
+    def __init__(self, z_dim=32, stride=2, kernel_size = 3, padding = 1, output_padding = 1):
         super().__init__()
         self.z_dim = z_dim
+        self.stride = stride
+        self.kernel_size = kernel_size
+        self.padding = padding
 
         # BT pre_trained
         # we want to matrix output
         # shape ( batch, 32, 105, 145 )
         self.encoder = nn.Sequential(
-            nn.Conv2d(1, 16, kernel_size = 3 , stride = 2 , padding = 1),
+            nn.Conv2d(1, 16, kernel_size , stride , padding ),
             nn.ReLU(),
-            nn.Conv2d(16, 32, kernel_size = 3, stride = 2, padding = 1),
+            nn.Conv2d(16, z_dim, kernel_size, stride, padding),
             nn.ReLU(),
         )
 
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(z_dim, 16, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ConvTranspose2d(z_dim, 16, kernel_size, stride, padding, output_padding),
             nn.ReLU(),
-            nn.ConvTranspose2d(16, 1, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ConvTranspose2d(16, 1, kernel_size, stride, padding, output_padding),
             nn.Sigmoid()
         )
 
@@ -43,7 +47,7 @@ class BT_VAE(nn.Module):
 
     def decode(self, h):
         h = self.decoder(h)
-        h = F.interpolate( h, size=( 105, 145 ), mode='bilinear' )
+        h = F.interpolate( h, size=( 420, 580 ), mode='bilinear' )
         return h
 
     def get_CM(self, BT_mu, mu):
@@ -97,9 +101,36 @@ class BT_VAE(nn.Module):
     def VAE_Loss(recon, x, log_var, mu):
         recon_loss = F.binary_cross_entropy(recon, x, reduction='sum')
         kl_loss = -0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp())
+        beta = 1
 
-        return recon_loss, kl_loss
+        return recon_loss + kl_loss * beta
 
-    @staticmethod
+    @staticmethod 
     def get_Loss(VAE_Loss,BT_Loss):
         return VAE_Loss + BT_Loss
+
+    
+
+# model Testing
+if __name__ == '__main__':
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = BT_VAE(z_dim = 32)
+    model.to(device)
+    op = torch.optim.Adam(model.parameters(), lr=1e-3)
+
+    # glob always out list
+    # if i want to input just one file,
+    # i can use np.load
+    x = np.load('DataSet/Processed/1_/test/1.tif.npy')
+
+    # npy to tensor
+    x = torch.tensor(x).float()
+    x = x.unsqueeze(0)
+    x = x.to(device)
+    print(x.shape)
+
+
+    # print(x.shape)
+    CM, z, recon, log_var, mu = model(x)
+    print("CM:", CM.shape)
+    print("recon:", recon.shape)
